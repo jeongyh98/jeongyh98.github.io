427f46f63e94fb190323f2621752ac702e261ad5
jeongyh98 <46309123+jeongyh98@users.noreply.github.com>
2026-02-12 16:44:30 +0000

    Deploying to gh-pages from @ jeongyh98/jeongyh98.github.io@5e2c5f8f25e70716bec8bbdcb0886876fb33cc31 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jeongyh98/jeongyh98",
+    "data-repo": "jeongyh98/jeongyh98.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
